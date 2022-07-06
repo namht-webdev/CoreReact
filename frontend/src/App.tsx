c09af379ede0d62be9b3dba2f9ec0c59623cde8b
HOÀNG TRUNG NAM <18120466@student.hcmus.edu.vn>
2022-07-06 15:47:20 +0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from './components/Header';
+const App: React.FC = () => {
+  debugger;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+};
+
+export default App;

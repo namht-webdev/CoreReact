8715e85c4231163b62307b72e1e4410bfc85a0fe
HOÀNG TRUNG NAM <18120466@student.hcmus.edu.vn>
2022-07-13 16:13:53 +0700

    update

--- frontend/src/components/QuestionList.tsx
+++ frontend/src/components/QuestionList.tsx
@@ -11,7 +11,7 @@ export const QuestionList: FC<Props> = ({ data, renderItem }) => (
   <ul className="mt-3 mx-0 mb-0 py-0 px-5 list-none bg-white rounded-b-[4px] border-t-[3px] border-solid border-[#efd197] shadow-[0_3px_5px_0_rgba(0,0,0,0.16)]">
     {data.map((question) => (
       <li className="first-of-type:border-none border-t-[1px] boder-solid border-[#e3e2e2] w-full">
-        {renderItem ? renderItem(question) : <Question data={question} />}
+        <Question data={question} />
       </li>
     ))}
   </ul>

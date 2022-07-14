1e35e03611006bb7531c1623e2cca5c880a65e15
HOÀNG TRUNG NAM <18120466@student.hcmus.edu.vn>
2022-07-14 09:51:45 +0700

    update data to fetch with React Hooks

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,15 +1,35 @@
+import { useState, useEffect } from 'react';
 import { PrimaryButton } from './PrimaryButton';
 import { QuestionList } from './QuestionList';
-import { getUnansweredQuestions } from '../interfaces/QuestionsData';
+import {
+  getUnansweredQuestions,
+  QuestionData,
+} from '../interfaces/QuestionsData';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
 
-export const HomePage = () => (
-  <Page>
-    <div className="flex items-center justify-between">
-      <PageTitle>Unanswered Questions</PageTitle>
-      <PrimaryButton>Ask a question</PrimaryButton>
-    </div>
-    <QuestionList data={getUnansweredQuestions()} />
-  </Page>
-);
+export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[] | null>(null);
+  const [questionsLoading, setQuestionsLoading] = useState(true);
+  useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unanswerQuestions = await getUnansweredQuestions();
+      setQuestions(unanswerQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+  return (
+    <Page>
+      <div className="flex items-center justify-between">
+        <PageTitle>Unanswered Questions</PageTitle>
+        <PrimaryButton>Ask a question</PrimaryButton>
+      </div>
+      {questionsLoading ? (
+        <div className="text-[16px] italic">Loading ...</div>
+      ) : (
+        <QuestionList data={questions || []} />
+      )}
+    </Page>
+  );
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,16 @@
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import './index.css';
+const container = document.getElementById('root');
+// OH BOY, container can be null but createRoot can't handle this...
+if (!container) {
+  // eslint-disable-next-line no-throw-literal
+  throw "Can't instantiate";
+}
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
